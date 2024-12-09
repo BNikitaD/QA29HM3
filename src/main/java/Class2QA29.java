d3f1c2a50e404e330add0ac967ca32ae7faa7782
BNikitaD <nikitabulavko9@gmail.com>
2024-12-09 21:25:53 +0300

    Revert "Revert "Lesson 4""
    This reverts commit a11fa5ea980ee9810fa431519a0f0cef731d5e32.

--- src/main/java/Class2QA29.java
+++ src/main/java/Class2QA29.java
@@ -1,7 +1,34 @@
+import java.util.Arrays;
+
 public class Class2QA29 {
     public static void main(String[] args) {
-        int n = 10;
-        System.out.println("n");
+
+        int [] mass = new int[] {2,4,6,8,10};
+
+       int colichestvo = mass.length;
+       int sum = 0;
+       int max = 0;
+       int min = 0;
+
+       for (int number : mass){
+           sum=sum+number;
+       }
+       for(int number : mass){
+           if(number> max){
+               max = number;
+           }
+       }
+       for(int number : mass){
+           if(number<min){
+               min = number;
+           }
+       }
+
+
+       System.out.println(colichestvo);
+        System.out.println(sum);
+        System.out.println(max);
+        System.out.println(min);
 
     }
 }
