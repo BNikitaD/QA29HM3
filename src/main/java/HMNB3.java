39faf475dae6e596e562cccd50e97b625019348d
BNikitaD <nikitabulavko9@gmail.com>
2024-12-09 20:28:08 +0300

    Revert "QA29Nikita"

--- src/main/java/HMNB3.java
+++ src/main/java/HMNB3.java
@@ -1,14 +1,5 @@
-import java.util.Arrays;
-
 public class HMNB3 {
     public static void main(String[] args) {
-
-
-        int [] value1 = new int []{2,4,6,8,10};
-        int[] value2 = new int[]{1,3,5,7,9,11};
-
-        System.out.println(Arrays.toString(value1));
-        System.out.println(Arrays.toString(value2));
-
+        System.out.println("okay");
     }
 }
